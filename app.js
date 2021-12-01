d7997614bb77b92df86f154ba940e1b6127e1724
martinchiu <martin87713@gmail.com>
2021-12-01 10:00:56 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -25,23 +25,31 @@ app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 // 設定路由
-app.get('/', (req, res) => {
-  Todo.find()  // 取出 Todo model 裡的所有資料
-    .lean()    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos }))    // 將資料傳給 index 樣板
-    .catch(error => console.error(error))             // 錯誤處理
-})
-
-app.get('/todos/new', (req, res) => {
-  res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  return Todo.create({ name })          // 從 req.body 拿出表單裡的 name 資料
-    .then(() => res.redirect('/'))      // 存入資料庫
-    .catch(error => console.log(error)) // 新增完成後導回首頁
-})
+  // 首頁
+  app.get('/', (req, res) => {
+    Todo.find()  // 取出 Todo model 裡的所有資料
+      .lean()    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+      .then(todos => res.render('index', { todos }))    // 將資料傳給 index 樣板
+      .catch(error => console.error(error))             // 錯誤處理
+  })
+  // 新增todo
+  app.get('/todos/new', (req, res) => {
+    res.render('new')
+  })
+  app.post('/todos', (req, res) => {
+    const name = req.body.name
+    return Todo.create({ name })          // 從 req.body 拿出表單裡的 name 資料
+      .then(() => res.redirect('/'))      // 存入資料庫
+      .catch(error => console.log(error)) // 新增完成後導回首頁
+  })
+  // 瀏覽特定todo
+  app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then(todo => res.render('detail', { todo }))
+      .catch(error => console.log(error))
+  })
 
 // 啟動伺服器
 app.listen(3000, () => {
