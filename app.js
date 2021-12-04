651193e212ad50c95476a494ea53113f50652dab
martinchiu <martin87713@gmail.com>
2021-12-04 19:33:49 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -19,66 +19,67 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 // 引入express 內建的body-parser
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 // 設定模板引擎
-app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // 設定路由
-  // 首頁
-  app.get('/', (req, res) => {
-    Todo.find()  // 取出 Todo model 裡的所有資料
-      .lean()    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-      .then(todos => res.render('index', { todos }))    // 將資料傳給 index 樣板
-      .catch(error => console.error(error))             // 錯誤處理
-  })
-  // 新增todo
-  app.get('/todos/new', (req, res) => {
-    res.render('new')
-  })
-  app.post('/todos', (req, res) => {
-    const name = req.body.name
-    return Todo.create({ name })          // 從 req.body 拿出表單裡的 name 資料
-      .then(() => res.redirect('/'))      // 存入資料庫
-      .catch(error => console.log(error)) // 新增完成後導回首頁
-  })
-  // 瀏覽特定todo
-  app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then(todo => res.render('detail', { todo }))
-      .catch(error => console.log(error))
-  })
-  // 修改特定todo
-  app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then(todo => res.render('edit', { todo }))
-      .catch(error => console.log(error))
-  })
-  app.post('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    const { name, isDone} = req.body
+// 首頁
+app.get('/', (req, res) => {
+  Todo.find()  // 取出 Todo model 裡的所有資料
+    .lean()    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _ido: 'desc' })
+    .then(todos => res.render('index', { todos }))    // 將資料傳給 index 樣板
+    .catch(error => console.error(error))             // 錯誤處理
+})
+// 新增todo
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })          // 從 req.body 拿出表單裡的 name 資料
+    .then(() => res.redirect('/'))      // 存入資料庫
+    .catch(error => console.log(error)) // 新增完成後導回首頁
+})
+// 瀏覽特定todo
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+// 修改特定todo
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
 
-    return Todo.findById(id)
-      .then(todo => {
-        todo.name = name
-        todo.isDone = isDone === 'on'
-        return todo.save()
-      })
-      .then(() => res.redirect(`/todos/${id}`))
-      .catch(error => console.log(error))
-  })
-  // 刪除特定todo
-  app.post('/todos/:id/delete', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .then(todo => todo.remove())
-      .then(() => res.redirect('/'))
-      .catch(error => console.log(error))
-  })
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+// 刪除特定todo
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 // 啟動伺服器
 app.listen(3000, () => {
