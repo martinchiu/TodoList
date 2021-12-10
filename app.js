48216908c27af5df6df910e6c4861f1fbab51f94
martinchiu <martin87713@gmail.com>
2021-12-10 09:32:57 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const methodOverride = require('method-override')
 const routes = require('./routes')  // 引用路由器，這樣寫程式會自動去找routes 底下的index.js 檔案
 
 require('./config/mongoose')
+
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000 
 const app = express()
 
+const PORT = process.env.PORT || 3000
 
 // 引入express 內建的body-parser
 app.use(express.urlencoded({ extended: true }))
@@ -21,6 +25,6 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // 啟動伺服器
-app.listen(3000, () => {
-  console.log('app is running')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
+// 如果在 Heroku 環境則使用 process.env.MONGODB_URI
+// 否則為本地環境，使用 mongodb://localhost/todo-list
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+
 // 資料庫連線
-mongoose.connect('mongodb://localhost/todo-list')
+mongoose.connect(MONGODB_URI)
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
